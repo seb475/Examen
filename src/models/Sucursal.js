24b63560f2625e956a6828df94b2df1342ed7de9
SEBASTIAN PEDRAZA <12sebastianmx@gmail.com>
2026-04-27 19:18:41 -0600

    modelo sucursal agregado

---
+++ src/models/Sucursal.js
@@ -0,0 +1,18 @@
+const mongoose = required('mongoose');
+const { Schema } = mongoose;
+
+const sucursalSchema = new Schema({
+   nombre: {
+          type: String,
+          require: true,
+         } ,
+
+    ubicacion: {
+          type: String,
+          require: true,
+         }      
+
+});
+
+const Sucursal = mongoose.model('Sucursal', sucursalSchema);
+module.exports = Sucursal;

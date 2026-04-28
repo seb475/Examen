76d266a3eb15ee8091c99eee59bb58d1ae029ead
SEBASTIAN PEDRAZA <12sebastianmx@gmail.com>
2026-04-27 20:29:45 -0600

    rutas stock creadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const productoRouter = require('./prodcuto.routes');
 const sucursalRouter = require('./sucursal.routes');
 const movimientoRouter = require('./movimiento.routes');
+const stockRouter = require('./stock.routes');
 const router = express.Router();
 
 router.use(productoRouter);
 router.use(sucursalRouter)
 router.use(movimientoRouter)
+router.use(stockRouter)
 
 
 module.exports = router;
---
+++ src/routes/stock.routes.js
@@ -0,0 +1,15 @@
+const { obtenerStockPorSucursal,obtenerInventarioGeneral } = require('../controllers/stock.controller');
+const express = require('express');
+
+const stockRouter = express.Router();
+
+stockRouter.route('/')
+    .get(obtenerInventarioGeneral)
+   
+
+stockRouter.route('/:sucursalId')
+    .get(obtenerStockPorSucursal);
+
+
+
+module.exports = stockRouter;

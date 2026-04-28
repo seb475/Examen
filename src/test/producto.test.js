3e594c52c2f785a02e4a980b366df339bf1fbe52
SEBASTIAN PEDRAZA <12sebastianmx@gmail.com>
2026-04-28 12:25:47 -0600

    test de prodcutos creado e instalacion de mongodb-memory-server

---
+++ src/test/producto.test.js
@@ -0,0 +1,42 @@
+const  request  = require("supertest");
+const app = require("../app")
+const mongoose = require("mongoose")
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mongoServer
+let id
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create()
+    const uri = mongoServer.getUri()
+    await mongoose.connect(uri)
+});
+
+afterAll(async () => {
+    await mongoose.connection.dropDatabase()
+    await mongoose.connection.close()
+    await mongoServer.stop()
+});
+
+test('GET/ productos deve traer todos los prodcutos ', async () => {
+    const res = await request(app)
+    .get("/productos")
+    expect(res.status).toBe(200)
+    expect(res.body).toBeInstanceOf(Array)
+});
+
+
+test('POST / productos deve crear un producto', async () => {
+    const nuevoProducto = {
+        SKU: "TAMDRA-004",
+        nombre: "Tablones de Madera",
+        precio: "7830",
+        categoria: "Construccion"
+    }
+    const res = await request(app).post("/productos").send(nuevoProducto)
+    id = res.body._id || res.body.id; 
+    expect(res.status).toBe(201);
+    expect(id).toBeDefined(); 
+    expect(res.body.nombre).toBe(nuevoProducto.nombre) 
+})
+

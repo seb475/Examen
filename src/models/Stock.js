89ccbf5bc9731558c63f1f56e90ff9e552adfb7a
SEBASTIAN PEDRAZA <12sebastianmx@gmail.com>
2026-04-27 19:28:30 -0600

    modelo stock agregado

---
+++ src/models/Stock.js
@@ -0,0 +1,26 @@
+const mongoose = required('mongoose');
+const { Schema } = mongoose;
+
+const stockSchema = new Schema({
+  productoId: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Producto', 
+    required: true
+  },
+  sucursalId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Sucursal', 
+    required: true
+  },
+  cantidad: { 
+    type: Number, 
+    required: true, 
+    default: 0,
+    min: [0]
+  }      
+});
+
+stockSchema.index({ productoId: 1, sucursalId: 1 }, { unique: true });
+
+const Stock = mongoose.model('Stock', stockSchema);
+module.exports = Stock;
